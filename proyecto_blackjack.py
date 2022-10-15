f933282e05e7915a3863bb56549a8837cf08e44b
juanvalos <111295746+juanvalos@users.noreply.github.com>
2022-10-14 19:33:43 -0500

    Update proyecto_blackjack.py
    Este es el avance implementando funciones en mi programa

--- proyecto_blackjack.py
+++ proyecto_blackjack.py
@@ -1,24 +1,35 @@
-""""
-Este programa será complejo, sin embargo las operaciones de este son muy sencillas.
-Aquí solo se usará 2 operadores, el cual es "SUMA" (+) y "MODULO" (%).
-El operador de suma lo ocuparemos cuando la computadora vaya sumando los numeros de los jugadres
-y que cheque que no se excedan de 21.
-El operador de modulo lo ocuparé para saber los turnos de cada jugador. Para esto ocuparé una variable extra. en este ejemplo es (n)
-A esta variable se le irá sumando 1 cada vez que la función de jugar empiece y termine, despues de esto hallaremos el residuo sobre 2 de n+1
-Cuando el residuo sea 1, será turno del jugador 1
-Cuando el residuo sea 2, será el turno del jugador 2
 """
-#SUMA
-turno_1=10
-turno_2=5
-valor_final= turno_1+turno_2
-print(f"los puntos finales del juagador 1 son  {valor_final}, podemos continuar con el otro turno")
+    En este Avance crearé las funciones que ocuparé en mi proyecto, para esto usaré el codygo y los operadores del Avance 2
+"""
+import random
 
-#MODULO
-n=0
-turno=n+1
-if turno %2 == 1:
-    print("es turno del jugador 1")
 
-if turno %2 == 0:
-    print("es turno del jugador 2")
+#Esta es la función de la suma de las cartas
+def suma_cartas():
+
+    num1=random.randint(1,10)
+    num2=num1 + random.randint(1,10)
+
+    iniciar_juego=input("Bienvenidos a este juego de cartas llamado 21, si desea jugar presione 1: ")
+    
+    if iniciar_juego =="1" :
+        print (f"Su carta tiene un valor de {num1}")
+        cont_juego=input("Si desea continuar presione 1: ")
+
+        if cont_juego=="1":
+            print(f"La suma de sus numeros es {num2}")
+            
+#esta es la función del contador para el turno del jugador
+def contador(): 
+
+    n=0
+    turno=n+1
+
+    if turno %2 == 1:
+        print("es turno del jugador 1")
+
+        if turno %2 == 0:
+            print("es turno del jugador 2")
+
+suma_cartas()
+contador()
