adb706124e5dac707d6ce90e293b911601f8a449
juanvalos <111295746+juanvalos@users.noreply.github.com>
2022-10-14 19:47:35 -0500

    Update proyecto_blackjack.py
    En este avance se agregaron listas anidadas a mi proyecto

--- proyecto_blackjack.py
+++ proyecto_blackjack.py
@@ -1,134 +1,145 @@
 """
-En este avance implementaré listas a mi proyecto
+Juan Yael Avalos Mayorga // A01276329
+Este  es el código del juego 21, agregando listas anidadas
 """
 
 import random
 
+def print_menu () :
+    print (" ")
+    print ("Bienvenido al juego de 21 programado en Python :), le recuerdo que este juego es para 2 personas.")
+    print (" ")
+    print ("1. Empezar el Juego")
+    print (" ")
+    print ("2. Salir")
+    print (" ")
+    menu_choice = int (input ("Por favor eliga una opción: "))
+    print (" ")
+    return menu_choice
 
 
-# Definimos una función para imprimir el valor de que el jugador tiene con sus cartas
-
-def print_cart (num) :
-
-        print (f"Su cartas tienen un valor de {num}")
-        print (" ")
-
-
-
-# Definimos una función para el contador para el turno del jugador
-
-def contador() : 
-
-    n=0
-    turno=n+1
-
-    if turno %2 == 1:
-        print("es turno del jugador 1")
-
-        if turno %2 == 0:
-            print("es turno del jugador 2")
+def player_menu () :
+    print ("1. Tomar una carta")
+    print (" ")
+    print ("2. Dejar de tomar cartas")
+    print (" ")
+    player_choice = int (input  ("Eliga una de las siguientes opciones: "))
+    print (" ")
+    return player_choice
 
 
-
-# Definimos una función que sirva  para imprimir el valor final de las cartas del jugador
-
-def cart_s ( num ) :
-        print (f"El numero final de sus cartas es {num}")
-        print (" ")
-
-
-
-# Definimos una función para imprimir que el jugador se excedió en el valor de sus cartas
-def cart_e ( num ):
-        print (f"Usted se ha excedido del valor máximo para ganar con {num} en sus cartas")
-        print (" ")
-
-
-
-# Definimos una función para el juego para el jugador 1, implementando while
-
-def player1_game ():
+def choose_winner (lista_de_listas) :
     
-    print ( "Es turno del jugador 1, empecemos con su juego" )
-    print ( " " )
-    print_cart (num1)
-
-    i = 0
-    player_list = [num1]
-
-    while (i < 21) :
-        
-        cont_game = int (input("Si desea tomar otra carta presione 1, y si desea quedarse con el valor de sus cartas presione 2: "))
+    score_player1 = lista_de_listas [0][-1]
+    score_player2 = lista_de_listas [1][-1]
+    
+    if score_player1 > 21 :
+        score_player1 = 0
+    
+    if score_player2 > 21 :
+        score_player2 = 0
+    
+    if score_player1 > score_player2 :
+        print (" ")
+        print (" ")
+        print ("!!!El ganador del juego es el jugador 1!!!")
+        print (" ")
         print (" ")
 
-        if cont_game == 1 :
-            sum_cart= i + num1
-            i= sum_cart + random.randint(1,10)
-            print_cart (i)
-            player_list.append (i)
-            
-        if cont_game == 2 :
-            cart_s (i)
-            print (f"La evolución de su juego se ve así: {player_list}")
-            break
-
-        if i > 21 :
-            cart_e ( i )
-            print (f"La evolución de su juego se ve así: {player_list}")
-            break
-
- 
-    print ( " " )
-    print ( " " )
-    print ( " " )            
-
-
-# Definimos una función para el juego para el jugador 2, implementando while
-
-def player2_game ():
-     
-    print ( "Es turno del jugador 2, empecemos con su juego" )
-    print ( " " )
-    print_cart (num2)
-
-    i = 0
-    player_list = [num2]
-
-    while (i < 21) :
-        
-        cont_game = int (input("Si desea tomar otra carta presione 1, y si desea quedarse con el valor de sus cartas presione 2: "))
+    if score_player2 > score_player1 :
+        print (" ")
+        print (" ")
+        print ("!!!El ganador del juego es el jugador 2 !!!")
+        print (" ")
         print (" ")
 
-        if cont_game == 1 :
-            sum_cart= i + num2
-            i= sum_cart + random.randint(1,10)
-            print_cart (i)
-            player_list.append (i)
+
+
+options = print_menu()
+
+if options == 1:
+
+    turno = 0
+    acum_cart1 = 0
+    acum_cart2 = 0
+    player1_list = []
+    player2_list = []
+
+    while turno <= 20 :
+       
+        if turno % 2 == 0 :
+        
+            print (" ")
+            print ("Es turno del Jugador 1 ----->")
+            print (" ")
+            player_m = player_menu ()
             
-        if cont_game == 2 :
-            cart_s (i)
-            print (f"La evolución de su juego se ve así: {player_list}")
-            break
+            if player_m == 1 :
+                acum_cart1 += random.randint (1 , 10)
+                print (" ")
+                print (f"el valor de sus cartas es  {acum_cart1}")
+                print (" ")
+                player1_list.append (acum_cart1)
+            
+            if acum_cart1 > 21 :
+                print (" ")
+                print ("Jugador 1, usted se ha superado el límite de valor en sus cartas. Este juego ha acabado.")
+                print (" ")
+                break
 
-        if i > 21 :
-            cart_e ( i )
-            print (f"La evolución de su juego se ve así: {player_list}")
-            break
+            if player_m == 2 :
+                print (" ")
+                print (f"el valor final de sus cartas es  {acum_cart1}")
+                print (" ")
+                break
 
- 
-    print ( " " )
-    print ( " " )
-    print ( " " )            
 
-# Aqui iniciamos el Juego
 
-in_game = int (input("Bienvenidos a este juego de cartas llamado 21, si desea jugar presione 1: "))
-print (" ")
+        if turno % 2 == 1 :
+        
+            print (" ")
+            print (" ")
+            print ("Es turno del Jugador 2 ----->")
+            print (" ")
+            player_m = player_menu ()
+            
+            if player_m == 1 :
+                acum_cart2 += random.randint (1 , 10)
+                print (" ")
+                print (f"el valor de sus cartas es  {acum_cart2}")
+                print (" ")
+                player2_list.append (acum_cart2)
+            
+            if acum_cart2 > 21 :
+                print (" ")
+                print ("Jugador 2, usted se ha superado el límite de valor en sus cartas. Este juego ha acabado.")
+                print (" ")
+                break
 
-num1 = random.randint(1,10)
-num2 = random.randint(1,10)
+            if player_m == 2 :
+                print (" ")
+                print (f"el valor final de sus cartas es  {acum_cart2}")
+                print (" ")
+                break
 
-if in_game == 1 :
-   
-   player1_game()
-   player2_game()
+
+        turno +=1
+    
+    final_list = [player1_list , player2_list]
+
+    print (" ")
+    print (" ")
+    print (f"Jugador 1, el proceso de su juego se ve así:  {player1_list}")
+    print (" ")
+    print (f"Jugador 2, el proceso de su juego se ve así:  {player2_list}")
+    print (" ")
+
+    choose_winner (final_list)
+
+elif options == 2 :
+    print ("Hasta la próximaa :)")
+    print (" ")
+
+else :
+    print ("Error")
+    print (" ")
