bc494d9117168f083c549116ff8bb0addccf6744
juanvalos <111295746+juanvalos@users.noreply.github.com>
2022-10-20 22:07:51 -0500

    Update proyecto_blackjack.py

--- proyecto_blackjack.py
+++ proyecto_blackjack.py
@@ -1,12 +1,12 @@
 """
 Juan Yael Avalos Mayorga // A01276329
-Este  es el código del juego "Blackjack".
+Este  es el código final del juego "Blackjack".
 """
 
-import random
-from random import shuffle
+import random                     # Importamos la libreria "random", la cual es ofrecida por python y ofrece varias funciones que randomizan los elementos de diferentes áreas 
+from random import shuffle        # De la librería  "random", importamos shuffle, la cual nos servirá para randomizar la lista que simula la baraja de cartas.
 
-lista_barajas = (                 # Creamos una lista que simule la baraja de cartas
+lista_barajas = (                 # Creamos una variable que contenga una lista que simule la baraja de cartas
 [1] * 4 +  [2] * 4 +
 [3] * 4 + [4] * 4 +
 [5] * 4 + [6] * 4 +
@@ -16,7 +16,7 @@ lista_barajas = (                 # Creamos una lista que simule la baraja de ca
 [10] * 4 
 )
 
-shuffle (lista_barajas)           # Con esta función los elementos de la lista se ordenan al azar.
+shuffle (lista_barajas)           # Con la función "shuffle" en lista_barajas, se randomizan los elementos de la lista, por lo que cuando se tome una carta esta va a estar completamente al azar.
 
 def print_menu () :               # Se crea una función que imprima el menú del juego y retorne la opción que eligió el jugador.
     print (" ")
@@ -41,7 +41,7 @@ def player_menu () :              # Se crea una función que imprima el menu de 
     return player_choice
 
 
-def choose_winner (lista_de_listas) :     # Se crea una función que escoja al ganador del juego dependiendo su puntaje y si este se excede de 21
+def choose_winner (lista_de_listas) :     # Se crea una función que escoja al ganador del juego dependiendo su desarrollo de juego y si este se excede de 21, su puntaje es 0
     
     score_player1 = lista_de_listas [0][-1]
     score_player2 = lista_de_listas [1][-1]
@@ -68,26 +68,26 @@ def choose_winner (lista_de_listas) :     # Se crea una función que escoja al g
 
 
 
-options = print_menu()     # Aquí empieza el desarrollo del juego
+options = print_menu()     # Aquí empieza el desarrollo del juego, primero se imprime el menú dej juego.
 
-if options == 1:
+if options == 1:           # Si se decide empezar a jugar, el juego de blackjack empezará.
 
-    turno = 0
-    acum_card1 = 0
-    acum_card2 = 0
-    player1_list = []
-    player2_list = []
+    turno = 0              # Este acumulador definirá el turno del jugador
+    acum_card1 = 0         # Este acumulador, va a sumar el puntaje de las cartas del jugador 1
+    acum_card2 = 0         # Este acumulador, va a sumar el puntaje de las cartas del jugador 2
+    player1_list = []      # En esta lista se va a ir agregando los puntajes que vaya obteniendo el jugador 1
+    player2_list = []      # En esta lista se va a ir agregando los puntajes que vaya obteniendo el jugador 2
 
     while turno <= 20 :     # Creamos el código del juego dentro de un while con la variable turno para que respete los turnos de los jugadores
        
         if turno % 2 == 0 :   # Si el módulo de turno/2 es par, será el turno del jugador 1
         
-            print (" ")
+            print (" ")       # Se  imprime el menú del jugador 1
             print ("Es turno del Jugador 1 ----->")
             print (" ")
             player_m1 = player_menu ()
             
-            if player_m1 == 1 :     # Si el jugador decide tomar una carta, esta la tomará de la lista y la almacenará en un acumulador y en una lista,
+            if player_m1 == 1 :     # Si el jugador 1 decide tomar una carta, esta la tomará de la lista, la borrará de esta, y la almacenará en un acumulador y en una lista,
                                     # esto sirve para cuando se escoja a un ganador
                 acum_card1 += lista_barajas.pop ()
                 print (" ")
@@ -109,13 +109,14 @@ if options == 1:
 
         if turno % 2 == 1 :     # Si el módulo de turno/2 es impar, será el turno del jugador 2.
         
-            print (" ")
+            print (" ")         # Se imprime el menú del jugador 2
             print (" ")
             print ("Es turno del Jugador 2 ----->")
             print (" ")
             player_m2 = player_menu ()
             
-            if player_m2 == 1 :    # Si el jugador decide tomar una carta, esta la tomará de la lista y la almacenará en un acumulador y en una lista.
+            if player_m2 == 1 :    # Si el jugador 2 decide tomar una carta, esta la tomará de la lista, la borrará de esta, y la almacenará en un acumulador y en una lista,
+                                    # esto sirve para cuando se escoja a un ganador
                 acum_card2 += lista_barajas.pop ()
                 print (" ")
                 print (f"el valor de sus cartas es  {acum_card2}")
@@ -148,7 +149,7 @@ if options == 1:
     print (f"Jugador 2, el proceso de su juego se ve así:  {player2_list}")
     print (" ")
 
-    choose_winner (final_list)      # Usamos la función "choose_winner" para elegir el ganador del juego
+    choose_winner (final_list)      # Usamos la función "choose_winner" con la lista de los 2 procesos de los jugadores para elegir el ganador del juego
 
 elif options == 2 :
     print ("Hasta la próximaa :)")
